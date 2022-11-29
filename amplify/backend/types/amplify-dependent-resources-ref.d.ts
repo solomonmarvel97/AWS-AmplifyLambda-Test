b3f04dbdfeab7daa4269b7de54f2cbce5db15347
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-11-29 15:59:11 +0100

    added git repo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "amplifytestGetBook": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "amplifytest": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
